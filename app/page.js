201d8862a358306e99e547ab92e5e05d15ddc83e
Pankaj Joshi <pankajjoshi4467@gmail.com>
2025-10-16 11:24:22 +0530

    Authentication Done

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>hi pankaj </div>
+    <Button variant="destructive" > hi</Button>
   );
 }
